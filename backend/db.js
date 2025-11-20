ec84b8530776286748de3f76074115cf789e08c8
elizabeth860 <ag.ad.primer.nivel@gmail.com>
2025-11-19 21:03:33 -0600

    Actualizando db.js para Railway

--- backend/db.js
+++ backend/db.js
@@ -5,9 +5,11 @@
 
 import mysql from "mysql2/promise";
 
-export const db = mysql.createPool({
-  host: process.env.MYSQL_HOST || "localhost",
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "",
-  database: process.env.MYSQL_DATABASE || "pred_diag_emocional",
+export const db = await mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
+
